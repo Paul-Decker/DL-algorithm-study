cb08fbcee3c53893dc9d00ee1d15f04b1cc0113e
PaulDecker <pauldecker@qq.com>
2024-03-22 17:33:47 +0800

    modify in 回归模型

--- 01_回归模型/liner_regression.py
+++ 01_回归模型/liner_regression.py
@@ -24,9 +24,9 @@ def step_gradient(b_current, w_current, points, learningRate):
     b_gradient = 0
     w_gradient = 0
     N = float(len(points))
-    for i in range(0, N):
-        x = points(i, 0)
-        y = points(i, 1)
+    for i in range(0, int(N)):
+        x = points[i, 0]
+        y = points[i, 1]
         # 计算梯度值
         # 计算所有点的梯度值，求平均
         b_gradient += (2/N) * (((w_current*x)+b_current) - y)
@@ -47,9 +47,9 @@ def gradient_descent_runner(points, starting_b, starting_w, learningRate, num_it
 
 def run():
     # 读取数据
-    points = np.genfromtxt('data.csv', delimiter=',')
+    points = np.genfromtxt('./01_回归模型/data.csv', delimiter=',')
     # 设置学习率
-    learningRate = 0.001
+    learningRate = 0.0001
     # 初始化参数
     initial_b = 0
     initial_w = 0
@@ -58,9 +58,9 @@ def run():
     print(f'starting gradient descent at b = {initial_b}, w = {initial_w}, '
         + f'error/loss = {compute_error_for_line_given_points(initial_b, initial_w, points)}')
     print('Running...')
-    [b, m] = gradient_descent_runner(points, initial_b, initial_w, learningRate, num_iteration)
+    [b, w] = gradient_descent_runner(points, initial_b, initial_w, learningRate, num_iteration)
     print(f'After {num_iteration} iterations b = {b}, w = {w}, '
-        + f'error = {compute_error_for_line_given_points(b, m, points)}')
+        + f'error = {compute_error_for_line_given_points(b, w, points)}')
     
-if __name__ == 'main':
+if __name__ == '__main__':
     run()
